82055a6683c9cafa9e3e1fb7f48975cb9b3192e6
Rohit Pawar <friends9850@gmail.com>
2023-06-13 10:33:42 +0530

    object literals

--- second-react-app/src/App.js
+++ second-react-app/src/App.js
@@ -1,25 +1,16 @@
-function App() {
-  let str = `Hello Universe!!!`;
-  let pi = 3.141159;
-  let active = true;
-  let isUserdLoggedIn = true;
-
-  // JSX
+function App()
+{
+  let str =`Hello World`;
+  let user = {id : 10 ,name : "Rohit"};
+  let user1 = {id : 20 ,name : "Shubham"};
+  // object literal 
   return (
     <>
-      <h1>Hello World</h1>
-      <h1>{str}</h1>
-      <h1>PI {pi} !!</h1>
-
-      {/**IF Logged In */}
-      {isUserdLoggedIn && <h1>Welcome CDAC!</h1>}
-
-      {/** Usage of Boolean */}
-      <h1>{active ? "good morning" : "good night"}</h1>
-
-      {/** MOST USEFUL  */}
-      {active && <h1>Good Morning</h1>}
-      {active ? <h1>Good Morning</h1> : <h1>Good Night</h1>}
+    <h1>{str}</h1>
+    <h1>ID : {user.id}</h1>
+    <h1>Name : {user.name}</h1>
+    <h2>ID : {user1.id}</h2>
+    <h2>Name : {user1.name}</h2>
     </>
   );
 }
