9dc4eb0b29e2b393492ed2d56d03cbe5c592f2fc
Rohit Pawar <friends9850@gmail.com>
2023-06-13 10:29:12 +0530

    b8-inerpolation condition

--- second-react-app/src/App.js
+++ second-react-app/src/App.js
@@ -1,24 +1,27 @@
-function App ()
-{
-    return(
+function App() {
+  let str = `Hello Universe!!!`;
+  let pi = 3.141159;
+  let active = true;
+  let isUserdLoggedIn = true;
+
+  // JSX
+  return (
     <>
-    <h1>Hello World</h1>
-    <h3>Suggestions</h3>
-    <h1>/* 
-if auto suggestions are not working
-then on bottom of vs code select javascript
-and select javascript and jsx.
-*/</h1>
+      <h1>Hello World</h1>
+      <h1>{str}</h1>
+      <h1>PI {pi} !!</h1>
+
+      {/**IF Logged In */}
+      {isUserdLoggedIn && <h1>Welcome CDAC!</h1>}
+
+      {/** Usage of Boolean */}
+      <h1>{active ? "good morning" : "good night"}</h1>
+
+      {/** MOST USEFUL  */}
+      {active && <h1>Good Morning</h1>}
+      {active ? <h1>Good Morning</h1> : <h1>Good Night</h1>}
     </>
-    );
+  );
 }
 
 export default App;
-
-
-
-/* 
-if auto suggestions are not working
-then on bottom of vs code select javascript
-and select javascript and jsx.
-*/
