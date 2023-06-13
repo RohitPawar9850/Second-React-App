490039cd4a6321316fb59f71398ebe6a10652c5c
Rohit Pawar <friends9850@gmail.com>
2023-06-13 09:36:48 +0530

    function with parenthesis

--- second-react-app/src/App.js
+++ second-react-app/src/App.js
@@ -1,10 +1,14 @@
+//JSX :: return HTML
+//Syntax : <h1>Hello</h1>
+// Syntax : ( <h1>Hello</h1> )
+
 function App()
 {
   return(
-    <p>
+    <>
     <h1>Hello World</h1>
-    <h2>How are you</h2>
-    </p>
+    <p>A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long.</p>
+    </>
   );
 }
 export default App;
