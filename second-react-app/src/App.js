f6f2de7d524e555662b230332ad286ba69e7cc44
Rohit Pawar <friends9850@gmail.com>
2023-06-13 11:45:08 +0530

    inline styling

--- second-react-app/src/App.js
+++ second-react-app/src/App.js
@@ -1,18 +1,15 @@
-function App()
+import "./App.css";
+
+function App ()
 {
-  let str =`Hello World`;
-  let user = {id : 10 ,name : "Rohit"};
-  let user1 = {id : 20 ,name : "Shubham"};
-  // object literal 
   return (
     <>
-    <h1>{str}</h1>
-    <h1>ID : {user.id}</h1>
-    <h1>Name : {user.name}</h1>
-    <h2>ID : {user1.id}</h2>
-    <h2>Name : {user1.name}</h2>
+      <h1 style={{color :'red',background:'yellow'}}>Hello </h1>
     </>
   );
+
 }
 
 export default App;
+
+//for inline styling {{}} use 2 curly braces.
