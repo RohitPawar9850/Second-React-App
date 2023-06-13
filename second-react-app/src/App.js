0445b9fddc59f8e20d8c294a02aea3339d2ba16a
Rohit Pawar <friends9850@gmail.com>
2023-06-13 09:46:35 +0530

    conditional in jsx using variable

--- second-react-app/src/App.js
+++ second-react-app/src/App.js
@@ -1,22 +1,12 @@
-//JSX :: return HTML
-//Syntax : <h1>Hello</h1>
-// Syntax : ( <h1>Hello</h1> )
+// conditional in jsx
 
 function App()
 {
-  let jsx1 = <h1>Hello Universe</h1>
-
-  //multilinear with required parameter
-  let jsx2= (
-    <>
-      <h1>Hello World</h1>
-      <p>
-        <h2>I am Rohit Pawar</h2>
-        <h2>I am From Shevgaon.</h2>
-      </p>
-    </>
+  let jsx1 = <h1>Hello World</h1>
+  let jsx2 = (
+    <h1>Good Night</h1>
   );
-  return jsx2;
+  return false ? jsx1 : jsx2;
 }
 export default App;
 // new function
